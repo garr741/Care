e6f69b89b3ed795ee29d7ad344eff4386d57ee61
Tylor Garrett <tylorgarrett@gmail.com>
2017-06-17 14:26:12 -0400

    asdf

--- app/src/main/java/tgprojects/xyz/care/Activity/MessagesActivity.java
+++ app/src/main/java/tgprojects/xyz/care/Activity/MessagesActivity.java
@@ -24,7 +24,6 @@ public class MessagesActivity extends AppCompatActivity implements MessageThread
 
     @BindView(R.id.send_message) ImageButton sendMessage;
 
-
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
