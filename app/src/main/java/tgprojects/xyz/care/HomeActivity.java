b1b07381795344605792f222bd8205f870a5ab62
Tylor Garrett <tylorgarrett@gmail.com>
2017-06-17 00:37:42 -0400

    home stuff

--- app/src/main/java/tgprojects/xyz/care/HomeActivity.java
+++ app/src/main/java/tgprojects/xyz/care/HomeActivity.java
@@ -3,10 +3,10 @@ package tgprojects.xyz.care;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
